e8a4947087d883edc524119e23a7b62a8e2c0f1c
Dennis Pfisterer <github@farberg.de>
2020-07-01 13:38:05 +0200

    Add more command line options

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const log4js = require('log4js')
 const { program: optionparser } = require('commander')
 
-const CrHandler = require("./cr-handler.js");
+const Mk8sCrHandler = require("./mk8s-cr-handler.js");
 const Reconciler = require("./reconciler");
 
 // ------------------------------------------------
@@ -13,8 +13,12 @@ let options = optionparser
 	.option('-v, --verbose', "Display verbose output", false)
 	.option('--crd-file <filename>', 'CRD filename', './config/micro-k8s-crd-def-v1beta1.yaml')
 	.option('--namespace <ns>', 'Kubernetes namespace', 'default')
-	.option('--reconcile-interval <ms>', "Reconcile interval in millis", 10000)
+	.option('--reconcile-interval <ms>', "Reconcile interval in millis", 5 * 1000)
 	.option('--mode <mode>', 'The mode to start the app in (development or production)', 'production')
+	.option('--hostname <hostname>', 'Hostname to use for constructing the status report URL', 'localhost')
+	.option('--port <port>', 'Port to start the status collection server on', 8080)
+	.option('--image <image>', 'Docker image (and tag) to use', 'farberg/edsc-microk8s-playbook')
+	.option('--image-pull-policy', 'Image pull policy to use for the pod spec', 'IfNotPresent')
 	.version('0.0.1alpha')
 	.addHelpCommand()
 	.parse()
@@ -49,7 +53,7 @@ async function main(options) {
 	logger.debug(`Starting with options`, options)
 
 	//Create reconciler
-	const reconcileHandler = new CrHandler(Object.assign({}, { getLogger }, options));
+	const reconcileHandler = new Mk8sCrHandler(Object.assign({}, { getLogger }, options));
 	const reconciler = new Reconciler(Object.assign({}, { getLogger, reconcileHandler }, options))
 	await reconciler.start();
 }
